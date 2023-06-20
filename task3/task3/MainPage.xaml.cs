4ce376962d794c0bdf7b8bd5175fe52377062642
razanwael1122 <razanwael1122@gmail.com>
2023-06-20 11:12:08 +0300

    Add project files.

---
+++ task3/task3/MainPage.xaml.cs
@@ -0,0 +1,123 @@
+﻿using Plugin.Media.Abstractions;
+using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Microsoft.WindowsAzure.Storage;
+
+namespace task3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private MediaFile _mediaFile;
+        private string URL { get; set; }
+
+        //Picture choose from device    
+        private async void btnSelectPic_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Error", "This is not support on your device.", "OK");
+                return;
+            }
+            else
+            {
+                var mediaOption = new PickMediaOptions()
+                {
+                    PhotoSize = PhotoSize.Medium
+                };
+                _mediaFile = await CrossMedia.Current.PickPhotoAsync();
+                if (_mediaFile == null) return;
+                imageView.Source = ImageSource.FromStream(() => _mediaFile.GetStream());
+                UploadedUrl.Text = "Image URL:";
+            }
+        }
+
+        //Upload picture button    
+        private async void btnUpload_Clicked(object sender, EventArgs e)
+        {
+            if (_mediaFile == null)
+            {
+                await DisplayAlert("Error", "There was an error when trying to get your image.", "OK");
+                return;
+            }
+            else
+            {
+                UploadImage(_mediaFile.GetStream());
+            }
+        }
+
+        //Take picture from camera    
+        private async void btnTakePic_Clicked(object sender, EventArgs e)
+        {
+
+            await CrossMedia.Current.Initialize();
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":(No Camera available.)", "OK");
+                return;
+            }
+            else
+            {
+                _mediaFile = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    Directory = "Sample",
+                    Name = "myImage.jpg"
+                });
+
+                if (_mediaFile == null) return;
+                imageView.Source = ImageSource.FromStream(() => _mediaFile.GetStream());
+                var mediaOption = new PickMediaOptions()
+                {
+                    PhotoSize = PhotoSize.Medium
+                };
+                UploadedUrl.Text = "Image URL:";
+            }
+        }
+
+        //Upload to blob function    
+        private async void UploadImage(Stream stream)
+        {
+            Busy();
+            var account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=cloudshell2090062606;AccountKey=WLNkI+zDqqtsqWLIVTba5w2KbufN6rN1Tdzo7d1c/tIXSzq6gnCNHRPRT6hF5EuRjoVoGTyN9vMa+AStPW9eIg==;EndpointSuffix=core.windows.net");
+            var client = account.CreateCloudBlobClient();
+            var container = client.GetContainerReference("images");
+            await container.CreateIfNotExistsAsync();
+            var name = Guid.NewGuid().ToString();
+            var blockBlob = container.GetBlockBlobReference($"{name}.png");
+            await blockBlob.UploadFromStreamAsync(stream);
+            URL = blockBlob.Uri.OriginalString;
+            UploadedUrl.Text = URL;
+            NotBusy();
+            await DisplayAlert("Uploaded", "Image uploaded to Blob Storage Successfully!", "OK");
+        }
+
+        public void Busy()
+        {
+            uploadIndicator.IsVisible = true;
+            uploadIndicator.IsRunning = true;
+            btnSelectPic.IsEnabled = false;
+            btnTakePic.IsEnabled = false;
+            btnUpload.IsEnabled = false;
+        }
+
+        public void NotBusy()
+        {
+            uploadIndicator.IsVisible = false;
+            uploadIndicator.IsRunning = false;
+            btnSelectPic.IsEnabled = true;
+            btnTakePic.IsEnabled = true;
+            btnUpload.IsEnabled = true;
+        }
+    }
+}
